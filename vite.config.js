318876304d8e8012d41010e9e59335cda5817dda
ChauxNeuveTourisme <your-email@domain.com>
2025-04-23 19:52:57 +0200

    setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ChauxNeuveTourisme/', // <-- adjust this to your repo name!
+  base: '/ChauxNeuveTourisme.github.io/', // <-- adjust this to your repo name!
   plugins: [react()],
 });
